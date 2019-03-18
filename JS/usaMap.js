5ee1144268b9c4c3d5211f667d41f47d779ecee6
Noah Sterling <nas95@cornell.edu>
2019-03-17 21:25:18 -0400

    most of the map work completed

--- JS/usaMap.js
+++ JS/usaMap.js
@@ -2,9 +2,9 @@ let usaMap = d3.select("#usa");
 let svgMapWidth = usaMap.attr("width");
 let svgMapHeight = usaMap.attr("height");
 let svgMapMargin = {
-    top: 10,
+    top: 0,
     right: 20,
-    bottom: 10,
+    bottom: 0,
     left: 0
 };
 const mapWidth = svgMapWidth - svgMapMargin.left - svgMapMargin.right;
@@ -13,13 +13,14 @@ const map = usaMap.append("g")
     .attr("transform", "translate(" + svgMapMargin.left + "," + svgMapMargin.top + ")");
 
 
+let degree = "F";
 
 
 const geoData = async () => {
 
     //Geo variables
     const usa = await d3.json("Data/us.json");
-   // console.log(usa);
+    // console.log(usa);
 
     const stateIDs = await d3.tsv("Data/us-state-names.tsv");
     console.log(stateIDs);
@@ -42,16 +43,13 @@ const geoData = async () => {
         .attr("d", path)
         .attr("ident", d => d.id);
 
-    // if (d => d.attr("ident")=== 2){
-    //         d.remove();
-    // }    
 
-    // let statess = d3.selectAll(".state");
-    // statess.forEach(d=> {
-    //     if (d.attr("ident")=== 2){
-    //         d.remove();
-    //     }
-    // });
+    console.log("states");
+    console.log(d3.selectAll(".state"));
+
+    let stateDrawings = d3.selectAll(".state");
+    stateDrawings._groups[0][1].remove();
+    stateDrawings._groups[0][11].remove();
 
 
     map.append("path")
@@ -61,119 +59,103 @@ const geoData = async () => {
 
 
 
-    let testdata = await d3.csv("Data/combinedWeather.csv", d3.autoType);
+    let fulldata = await d3.csv("Data/combinedWeather.csv", d3.autoType);
 
-    console.log(testdata);
+    console.log(fulldata);
 
-    let activeYear=2018;
-    let activeMonth=4;
+    let activeYear = 2015;
+    let activeMonth = 4;
 
-   // data = data.filter(d => d['Year'] === activeYear && d['Month'] === activeMonth);
-    testdata = testdata.filter(d => d['YEAR'] === activeYear && d['MONTH'] === activeMonth);
-    console.log(testdata);
+
+    activeData = fulldata.filter(d => d['YEAR'] === activeYear && d['MONTH'] === activeMonth);
+    console.log(activeData);
 
     let stateTemps = {};
     let idToState = {};
+    let stateToData = {};
     stateIDs.forEach(row => {
         stateTemps[row.name] = 0;
         idToState[row.id] = row.name;
     });
     console.log(idToState);
-    console.log(testdata);
+    console.log(activeData);
     console.log(stateTemps);
 
-    
-    testdata.forEach( row => {
+
+    activeData.forEach((row, i) => {
         stateTemps[row.STATE] = row.AvgValue;
+        stateToData[row.STATE] = i;
     });
     console.log(stateTemps);
+    console.log(stateToData);
 
 
-    const minMax = d3.extent(testdata, d => d.AvgValue);
+
+
+    const minMax = d3.extent(activeData, d => d.AvgValue);
     console.log(minMax);
 
-   // console.log("HERE");
-
-  //  console.log(d3.values(idToState));
-  //  console.log(testdata);
 
 
-//   let colorScale = d3.scaleLinear()
-//   .domain(minMax)
-//   .range(["blue", "red"])
-//   .clamp(true)
-//   .interpolate(d3.interpolateHcl);
+    //   let colorScale = d3.scaleLinear()
+    //   .domain(minMax)
+    //   .range(["blue", "red"])
+    //   .clamp(true)
+    //   .interpolate(d3.interpolateHcl);
 
-
-    
-//   map.selectAll(".state")
-//   .style("fill", d => colorScale(testdata.get(d.id)));
-   // const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    // const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 
     //const colorScale = d3.scaleSequential(d3.interpolateRdBu).domain(minMax);
+
     // const colorScale = d3.scaleQuantile()
     //     .domain(d3.values(d3.values(stateTemps)))
     //     .range(["blue", "lightblue","red"]);
+
     const colorScale = d3.scaleQuantize()
-                          .domain(minMax)
-                          .range(["blue","lightblue","lightpink", "red"]);
+        .domain(minMax)
+        .range(["blue", "lightblue", "lightpink", "red"]);
 
-                  
+
     map.selectAll(".state")
-    .style("fill", d => colorScale( stateTemps[ idToState[d.id] ] ));
-
-    // let mapStates = map.selectAll(".state");
-    // console.log(mapStates);
-    // mapStates.forEach((d) => {
-    //     console.log(d3.this);
-    // });
-
-
-    // map.selectAll(".state")
-    //     //.style("fill", "blue");
-    //     .style("fill", colorScale(200));
-
-
-        // console.log("Test");
-        // console.log(colorScale(d => d.Temp));
-        // console.log( d => colorScale(d.Temp));
-        // console.log(colorScale(20));
-        // console.log(colorScale(30));
-        // console.log(colorScale(40));
-        // console.log(colorScale(1000));
-
+        .style("fill", d => colorScale(stateTemps[idToState[d.id]]));
 
 
     var hoverBox = d3.select("body").append("div")
-    .attr("class", "hoverBox")
-    .style("opacity", 0)
-    //.style("background-color", "blue");
+        .attr("class", "hoverBox")
+        .style("opacity", 0)
 
     d3.selectAll(".state").on("mousemove", mouseOnPlot);
     d3.selectAll(".state").on("mouseout", mouseLeavesPlot);
+    d3.selectAll(".outline").on("mouseover", overlay);
+    d3.selectAll(".outline").on("mouseout", mouseLeavesPlot);
 
+    var hoveBoxWidth = parseFloat(hoverBox.style("width"));
+    var hoverBoxHeight = parseFloat(hoverBox.style("height"));
 
     function mouseOnPlot() {
-       // console.log("move");
-        hoverBox.style("left", event.pageX)
-            .style("top", event.pageY)
+        hoverBox.style("left", event.pageX - (hoveBoxWidth/2) +"px")
+            .style("top", event.pageY - (hoverBoxHeight+20)+"px")
             .html("")
             .style("opacity", 1);
 
+
         let state = d3.select(this);
-        hoverBox.append("div").text(idToState[state.attr("ident")]);
-        console.log("test");
-        console.log(testdata[0]);
-        //hoverBox.append("div").text( testdata.idToState[state.attr("ident")] );
+        let dataRow = activeData[stateToData[idToState[state.attr("ident")]]];
+        let degreeSymbol = String.fromCharCode(176);
+        hoverBox.append("div").text(idToState[state.attr("ident")] + " " + dataRow.AvgValue + degreeSymbol + degree);
+        hoverBox.append("div").text("Min Value " + dataRow.MinValue + degreeSymbol + degree);
+        hoverBox.append("div").text("Max Value " + dataRow.MaxValue + degreeSymbol + degree);
+
     }
 
     function mouseLeavesPlot() {
         hoverBox.style("opacity", 0);
-       // console.log("Out");
     }
 
-    // console.log(map.selectAll("path"));
+    function overlay() {
+       hoverBox.style("opacity", 1);
+    }
 
 
 
@@ -182,81 +164,38 @@ const geoData = async () => {
 
 
 
+    //  Credit Prof. Rz. Adapted as needed
+    const legend = d3.select("#usaMapLegend");
+    const legendWidth = legend.attr("width");
+    const legendHeight = legend.attr("height");
+    const barHeight = 30;
+    const stepSize = 5; 
 
+    const pixelScale = d3.scaleLinear()
+                            .domain([0,legendWidth])
+                            .range([minMax[0],minMax[1]]); // In this case the "data" are pixels, and we get numbers to use in colorScale
+    const barScale = d3.scaleLinear()
+                        .domain([minMax[0],minMax[1]])
+                        .range([0,legendWidth]);
+    const barAxis = d3.axisBottom(barScale);
 
-
-
-
-
-
-
-
-
-    // // 3b. Generate the counts we will need
-    // let stateCounts = {};
-    // let idToState = {};
-    // stateIDs.forEach( row => {
-    //   stateCounts[row.name] = 0;
-    //   idToState[row.id] = row.name;
-    // });
-    // console.log(stateCounts);
-
-
-
-
-    // // 4. MARCH 11 -- Let's add a mouseover tooltip
-    // var roundNumber = d3.format(".5g");
-    // var tooltip = d3.select("body").append("div")
-    //                   .attr("id", "tooltip")
-    //                   .attr("class", "tooltip")
-    //                   .style("opacity", 0);
-    // var tooltipWidth = parseFloat(tooltip.style("width"));
-    // var tooltipHeight = parseFloat(tooltip.style("height"));
-
-    // d3.selectAll(".state").on("mousemove", mouseOnPlot);
-    // d3.selectAll(".state").on("mouseout", mouseLeavesPlot);
-
-    // function mouseOnPlot() {
-    //   // Move the tooltip
-    //   tooltip.style("left", event.pageX - (tooltipWidth/2.0));
-    //   tooltip.style("top", event.pageY - tooltipHeight - 24);
-
-    //   // Clear whatever is there
-    //   tooltip.html("");
-
-    //   // Give the tooltip a state label
-    //   //console.log(idToState);
-    //   let state = d3.select(this);
-    //   console.log(state);
-    //   tooltip.append("div").text(idToState[state.attr("ident")]);
-
-    //   // Give the tooltip a lat/lng
-    //   let posOnChart = d3.mouse(usaMap.node());
-    //   console.log(posOnChart);
-    //   let geoPos = projection.invert(posOnChart);
-    //   console.log(geoPos);
-    //   tooltip.append("div").text("("+roundNumber(geoPos[1])+","+roundNumber(geoPos[0])+")")
-
-    //   // Show the tooltip!
-    //   tooltip.style("opacity",1);
-    // }
-    // function mouseLeavesPlot() {
-    //   tooltip.style("opacity",0);
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    legend.append("g")
+      .attr("class", "legendAxis")
+      .style("stroke", "white")
+      .style("stroke-width", ".5px")
+      .attr("transform","translate("+(20)+","+(barHeight+5)+")")
+      .call(barAxis);
+      
+    // Draw rects of color down the bar
+    let bar = legend.append("g").attr("transform","translate("+(20)+","+(0)+")")
+    for (let i=0; i<legendWidth; i=i+stepSize) {
+      bar.append("rect")
+        .attr("x", i)
+        .attr("y", 0)
+        .attr("width", stepSize)
+        .attr("height",barHeight)
+        .style("fill", colorScale( pixelScale(i) )); // pixels => countData => color
+    }
 
 
 
