eed62966cd54133c8a5811e79be0f85807e2ab27
Noah Sterling <nas95@cornell.edu>
2019-03-25 12:31:34 -0400

    map colors

--- JS/usaMap.js
+++ JS/usaMap.js
@@ -63,12 +63,13 @@ const geoData = async () => {
 
     console.log(fulldata);
 
- //   let activeYear = 2015;
-  //  let activeMonth = 4;
+    //   let activeYear = 2015;
+    //  let activeMonth = 4;
 
 
-    function updateMap(activeYear, activeMonth){
+    function updateMap(activeYear, activeMonth) {
         activeData = fulldata.filter(d => d['YEAR'] === activeYear && d['MONTH'] === activeMonth);
+        console.log("DATA")
         console.log(activeData);
 
         let stateTemps = {};
@@ -78,24 +79,25 @@ const geoData = async () => {
             stateTemps[row.name] = 0;
             idToState[row.id] = row.name;
         });
-        console.log(idToState);
-        console.log(activeData);
-        console.log(stateTemps);
+        // console.log(idToState);
+        // console.log(activeData);
+        // console.log(stateTemps);
 
 
         activeData.forEach((row, i) => {
             stateTemps[row.STATE] = row.AvgValue;
             stateToData[row.STATE] = i;
         });
-        console.log(stateTemps);
-        console.log(stateToData);
+        //console.log(stateTemps);
+        // console.log(stateToData);
 
 
 
 
         const minMax = d3.extent(fulldata, d => d.AvgValue);
+        console.log("MinMax");
         console.log(minMax);
-    
+
 
 
         //   let colorScale = d3.scaleLinear()
@@ -107,15 +109,17 @@ const geoData = async () => {
         // const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 
-        //const colorScale = d3.scaleSequential(d3.interpolateRdBu).domain(minMax);
+       //const colorScale = d3.scaleSequential(d3.interpolateRdBu).domain(minMax);
 
         // const colorScale = d3.scaleQuantile()
         //     .domain(d3.values(d3.values(stateTemps)))
-        //     .range(["blue", "lightblue","red"]);
+        //     .range(["navy", "blue", "lightblue", "lightpink", "red","maroon"]);
 
         const colorScale = d3.scaleQuantize()
             .domain(minMax)
-            .range(["blue", "lightblue", "lightpink", "red"]);
+           // .range(["navy", "blue", "lightblue", "lightpink", "red",d3.rgb(98,34,40)]);
+            .range([d3.rgb(28,19,66), d3.rgb(46,63,111), d3.rgb(1,93,161), d3.rgb(57,159,220)
+                , d3.rgb(219,86,49), d3.rgb(200,45,40), d3.rgb(204,48,71), d3.rgb(98,34,40)]);
 
 
         map.selectAll(".state")
@@ -135,8 +139,8 @@ const geoData = async () => {
         var hoverBoxHeight = parseFloat(hoverBox.style("height"));
 
         function mouseOnPlot() {
-            hoverBox.style("left", event.pageX - (hoveBoxWidth/2) +"px")
-                .style("top", event.pageY - (hoverBoxHeight+20)+"px")
+            hoverBox.style("left", event.pageX - (hoveBoxWidth / 2) + "px")
+                .style("top", event.pageY - (hoverBoxHeight + 20) + "px")
                 .html("")
                 .style("opacity", 1);
 
@@ -155,7 +159,7 @@ const geoData = async () => {
         }
 
         function overlay() {
-           hoverBox.style("opacity", 1);
+            hoverBox.style("opacity", 1);
         }
 
 
@@ -170,35 +174,37 @@ const geoData = async () => {
         const legendWidth = legend.attr("width");
         const legendHeight = legend.attr("height");
         const barHeight = 30;
-        const stepSize = 5; 
+        const stepSize = 1;
 
         const pixelScale = d3.scaleLinear()
-                                .domain([0,legendWidth])
-                                .range([minMax[0],minMax[1]]); // In this case the "data" are pixels, and we get numbers to use in colorScale
+            .domain([0, legendWidth])
+            .range([minMax[0], minMax[1]]); // In this case the "data" are pixels, and we get numbers to use in colorScale
         const barScale = d3.scaleLinear()
-                            .domain([minMax[0],minMax[1]])
-                            .range([0,legendWidth]);
+            .domain([minMax[0], minMax[1]])
+            .range([0, legendWidth]);
         const barAxis = d3.axisBottom(barScale);
 
+        legend.html("");
+
         legend.append("g")
-          .attr("class", "legendAxis")
-          .style("stroke", "white")
-          .style("stroke-width", ".5px")
-          .attr("transform","translate("+(20)+","+(barHeight+5)+")")
-          .call(barAxis);
-          
+            .attr("class", "legendAxis")
+            .style("stroke", "white")
+            .style("stroke-width", ".5px")
+            .attr("transform", "translate(" + (20) + "," + (barHeight + 5) + ")")
+            .call(barAxis);
+
         // Draw rects of color down the bar
-        let bar = legend.append("g").attr("transform","translate("+(20)+","+(0)+")")
-        for (let i=0; i<legendWidth; i=i+stepSize) {
-          bar.append("rect")
-            .attr("x", i)
-            .attr("y", 0)
-            .attr("width", stepSize)
-            .attr("height",barHeight)
-            .style("fill", colorScale( pixelScale(i) )); // pixels => countData => color
+        let bar = legend.append("g").attr("transform", "translate(" + (20) + "," + (0) + ")")
+        for (let i = 0; i < legendWidth; i = i + stepSize) {
+            bar.append("rect")
+                .attr("x", i)
+                .attr("y", 0)
+                .attr("width", stepSize)
+                .attr("height", barHeight)
+                .style("fill", colorScale(pixelScale(i))); // pixels => countData => color
         }
-    
-}
+
+    }
     //https://jeffrz.github.io/info3300-spr2019/notes/19.03.11.notes.htm
 
 
@@ -216,23 +222,23 @@ const geoData = async () => {
 
     month_text.innerHTML = month;
     slidersDiv.append("div").text("Month")
-    .append("div").append("input")
-    .attr("type", "range").attr("class", "slider")
-    .attr("id","month")
-    .style("width","300px")
-    .attr("min", 0)
-    .attr("max", 11)
-    .attr("value", 0)
-    .on("input", function () {
-      // Whenever the slider changes, update intercept and chart
-      month = months[this.value];
-      console.log(this.value);
-      month_text.innerHTML = month;
-      updateMap(2015, Number(this.value)+1);
+        .append("div").append("input")
+        .attr("type", "range").attr("class", "slider")
+        .attr("id", "month")
+        .style("width", "300px")
+        .attr("min", 0)
+        .attr("max", 11)
+        .attr("value", 0)
+        .on("input", function () {
+            // Whenever the slider changes, update intercept and chart
+            month = months[this.value];
+            console.log(this.value);
+            month_text.innerHTML = month;
+            updateMap(2015, Number(this.value) + 1);
 
-    });
+        });
 
-   
+
 
 
 
@@ -240,7 +246,7 @@ const geoData = async () => {
 
 }
 
-   
+
 
 
 geoData();
